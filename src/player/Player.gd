bb75338100b816de5dabd8faef520ae5977013b0
Yudi Yamane <yudiyamane@gmail.com>
2023-03-11 16:03:28 -0300

    feat: adiciona som de recarregar e atirar

--- src/player/Player.gd
+++ src/player/Player.gd
@@ -9,6 +9,8 @@ extends CharacterBody2D
 
 @onready var animatedSprite := $AnimatedSprite
 @onready var weaponManager := $WeaponManager
+@onready var sfxHandgunReload := $SfxHandgunReload
+@onready var sfxHandgunShot := $SfxHandgunShot
 
 enum MovementState {
 	IDLE,
@@ -18,14 +20,19 @@ enum MovementState {
 
 enum WeaponState {
 	IDLE,
-	RELOAD,
 	SHOOT,
+	RELOAD,
 	MELEE,
 }
-@export var weapon_state := WeaponState.IDLE
+@export var weapon_state := WeaponState.IDLE:
+	set(new_state):
+		weapon_state = new_state
+		print('changing weapon state to: ', WeaponState.keys()[new_state])
 
 var Bullet = preload('res://src/weapon/Bullet.tscn')
 var movement := Vector2.ZERO
+var shot_sfx_timer = 0
+var last_shot_sfx = sfxHandgunShot
 
 func _ready() -> void:
 	_set_weapons_cooldown()
@@ -33,6 +40,7 @@ func _ready() -> void:
 	weapon_state = WeaponState.IDLE
 
 func _physics_process(delta: float) -> void:
+	shot_sfx_timer += delta
 	look_at(get_global_mouse_position())
 
 	movement = Vector2(
@@ -48,10 +56,10 @@ func _physics_process(delta: float) -> void:
 
 	move_and_slide()
 
-	if velocity == Vector2.ZERO:
+	if velocity == Vector2.ZERO and mov_state != MovementState.IDLE:
 		mov_state = MovementState.IDLE
 
-	_update_state()
+	_effect_state_change()
 
 
 func _input(event: InputEvent) -> void:
@@ -59,27 +67,36 @@ func _input(event: InputEvent) -> void:
 		if weapon_state != WeaponState.RELOAD and weapon_state != WeaponState.MELEE:
 			if weaponManager.shoot():
 				weapon_state = WeaponState.SHOOT
+				_play_sfx_handgun_shot()
+
 	elif event.is_action_pressed('reload'):
-		if weaponManager.reload():
-			weapon_state = WeaponState.RELOAD
+		if weapon_state != WeaponState.RELOAD:
+			if weaponManager.reload():
+				weapon_state = WeaponState.RELOAD
+				sfxHandgunReload.play()
+
 	elif event.is_action_pressed('melee'):
 		weapon_state = WeaponState.MELEE
 
-    # Isso vai atrapalhar a animação para MovmementState?
+	_effect_state_change()
+
+	# Isso vai atrapalhar a animação para MovmementState?
 	await animatedSprite.animation_finished
-	weapon_state = WeaponState.IDLE
+	if weapon_state != WeaponState.IDLE:
+		weapon_state = WeaponState.IDLE
 
-	_update_state()
 
-
-func _update_state():
+func _effect_state_change():
 	match weapon_state:
 		WeaponState.IDLE:
 			pass
+
 		WeaponState.SHOOT:
 			animatedSprite.play('HandgunShoot')
+
 		WeaponState.RELOAD:
 			animatedSprite.play('HandgunReload')
+
 		WeaponState.MELEE:
 			animatedSprite.play('HandgunMelee')
 
@@ -92,15 +109,25 @@ func _update_state():
 		MovementState.MOVE:
 			animatedSprite.play('HandgunMove')
 
-func _on_animated_sprite_animation_changed() -> void:
-	print('animation changed to ', animatedSprite.animation)
+func _play_sfx_handgun_shot():
+	if sfxHandgunShot.playing:
+		var new_gun_shot: AudioStreamPlayer = sfxHandgunShot.duplicate()
+		get_parent().add_child(new_gun_shot)
+		new_gun_shot.play()
+		print('>>> time ', shot_sfx_timer)
+		shot_sfx_timer = 0
+		await new_gun_shot.finished
+		new_gun_shot.queue_free()
+	else:
+		sfxHandgunShot.play()
+
 
 ## Tem que fazer algo parecido pra animação de melee e reload
 func _set_weapons_cooldown():
 	var animation_names = animatedSprite.sprite_frames.get_animation_names()
 	var shoot_animation_names = PackedStringArray()
 	var frames := .0
-	var fps = 0
+	var fps := 0
 	var anim_duration := .0
 	for anim_name in animation_names:
 		if anim_name.ends_with('Shoot'):
@@ -117,3 +144,8 @@ func _set_weapons_cooldown():
 		# print('          fps ', fps)
 		# print('anim_duration ', anim_duration)
 		# print()
+
+
+func _on_animated_sprite_animation_changed() -> void:
+	pass
+	print('animation changed to ', animatedSprite.animation)
